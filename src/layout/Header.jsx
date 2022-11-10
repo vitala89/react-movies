e168b045b70e8e36931ac7124e1af5dc48fd2618
vitalii kasap <vitala2089@gmail.com>
2022-11-10 23:47:28 +0100

    project complete

---
+++ src/layout/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return <div>
+        <nav className="purple darken-4">
+            <div className="nav-wrapper">
+                <a href="#" className="brand-logo">Movies</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="#">Repo</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+}
+
+export default Header;
